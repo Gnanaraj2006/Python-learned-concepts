d5a780dfa8c4f926bc639f261e082f19126c2df9
Gnanaraj2006 <krishdeveloper2006@gmail.com>
2024-06-09 14:24:33 +0530

    feat : Topics Datatypes and Methods for strings Learned

--- main.py
+++ main.py
@@ -1,1 +1,68 @@
-print("HelloWorld")
+#Source to learn : BRO CODE VIDEO STAMPS :
+#There are four Datatypes :
+
+#STRING
+
+first_name = "gnana"
+last_name = "raj"
+full_name = first_name+last_name
+print(full_name)
+
+
+#INTEGER
+age  = 20
+age += 1
+print("My age is :"+ str(age))
+
+
+#FLOAT
+
+height = 180.5
+print("My Height is :"+str(height)+"cm")
+
+
+#BOOLEAN
+
+human = True
+print("you are a human :"+ str(human))
+
+
+
+#Multiple assignments allows us to assign multiple variable at the same time in one line of code.
+name,age = "Krishna",17
+print(name)
+print(age)
+
+#Few Usefull Methods for strings
+name = "gnanaraj"
+print(len(name))
+print(name.capitalize())
+print(name.isalpha())
+print(name.isdigit())
+print(name.count("a"))
+print(name.find("g"))
+print(name.replace("a","o"))
+print(name.upper())
+print(name.lower())
+print(name*3)
+
+#Type Casting = convert the data type of a value to another datatype.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
